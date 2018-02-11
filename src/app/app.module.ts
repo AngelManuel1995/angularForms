8b2d1f4477bd17d2133f867b1c7793842a7f211f
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-02-10 22:33:46 -0500

    Components added to begin to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DataComponent } from './components/data/data.component';
+import { TemplateComponent } from './components/template/template.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataComponent,
+    TemplateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
