1d3a7ed65e7e578cf56f9a46e93f5d1b6fb86160
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-02-14 12:20:37 -0500

    validation of select tag its group of options

--- src/app/components/my-template/my-template.component.ts
+++ src/app/components/my-template/my-template.component.ts
@@ -1,22 +1,61 @@
 import { Component } from '@angular/core';
-
+import { Form }      from '@angular/forms';   
 @Component({
     selector:'app-mytemplate',
-    templateUrl:'./my-template.component.html'
+    templateUrl:'./my-template.component.html',
+    styles:[`
+        .ng-invalid.ng-touched:not(form){
+            border: 1px solid red;
+        }
+    `]
 })
 
 export class MyTemplateComponent{
 
     person:Object = {
-
+        name:null,
+        lastName:null,
+        email:null,
+        country:"Seleccione un pais",
+        gender:"M"
     }
+
+    contries:Object[] = [
+        {
+            name:"Colombia",
+            code:"COL"
+        },
+        {
+            name:"Chile",
+            code:"CHI"
+        },
+        {
+            name:"Argentina",
+            code:"ARG"
+        }
+    ]
+
+    /*
+        Podemos hacer esto para mantener un copia de un objeto original,
+        pero debemos hacerlo con el ngModel solo usando los parentesis.
+        person:Object = {
+            name:"Angel Manuel",
+            lastName:"Góez Giraldo",
+            email:"an@gmail.com"
+        }
+    */ 
 
     constructor(){
 
     }
 
-    send(forma:any){
+    send(forma:Form){
         console.log(forma);
+    }
+
+    show(personEmail:any){
+        console.log(personEmail);
+        console.log(personEmail.errors);
     }
 
 }
