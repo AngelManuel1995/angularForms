fb23715f131834a46cedc4fdf568ae1ab9b511f6
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-02-20 14:37:09 -0500

    Importing of FormGroup, Validator, FormControl from @angular/forms

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validator } from '@angular/forms';
 
 @Component({
     selector:'app-data',
@@ -7,8 +8,26 @@ import { Component } from '@angular/core';
 
 export class DataComponent{
     
-    constructor(){
+    forma:FormGroup;
 
+    constructor(){
+        this.forma = new FormGroup({
+            nombre: new FormControl('Angel'),
+            apellido: new FormControl(),
+            correo: new FormControl()
+        });
+    }
+
+    guardarCambios(){
+        console.log(this.forma.value);
     }
 
 }
+
+/**
+ * forma:FormGroup: es que el encarado de todos los elemento que vamos a utlilizar y recibe como parametros
+ * los elementos del furmulario.
+ * 
+ * new FormControl: es la clase mediante la cual instanciamos los elementos de los formularios.
+ * y resive como parámetro: 1- Valor por defecto, 2- Reglas de validación, y 3- reglas de validacion asincronicas
+ */
