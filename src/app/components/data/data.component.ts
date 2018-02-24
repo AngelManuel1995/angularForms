27afbeb82469a66ca96b62001dcee8e81dd96fc2
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-02-24 18:03:05 -0500

    Siple form done, now we'll make it more complicated

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -11,16 +11,24 @@ export class DataComponent{
     forma:FormGroup;
     constructor(){
         this.forma = new FormGroup({
-            nombre: new FormControl('', Validators.required ),
-            apellido: new FormControl('', Validators.required ),
-            correo: new FormControl('',[ 
-                                         Validators.required, 
-                                         Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] )
+            nombre: new FormControl('', 
+                                        [
+                                            Validators.required,
+                                            Validators.minLength(5)
+                                        ]),
+            apellido: new FormControl('', [
+                                            Validators.required,
+                                            Validators.minLength(5)
+                                        ]),
+            correo: new FormControl('', [ 
+                                            Validators.required, 
+                                            Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') 
+                                        ])
         });
     }
 
     guardarCambios(){
-        console.log(this.forma.value);
+        console.log(this.forma);
     }
 
 }
